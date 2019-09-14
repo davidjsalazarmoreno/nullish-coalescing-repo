9cbab9f7e45b468158d92dd0c3c88c4d44375498
David Salazar <davidjsalazarmoreno@gmail.com>
2019-09-14 14:58:06 -0500

    adds nullish examples

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const courseSettings = {
   id: 4,
   name: 'Javascript Course',
   author: 'CraftingCode',
-  shortName: '',
+  shortname: '',
   discount: 0,
   showPublictly: false,
   price: 100,
@@ -25,14 +25,19 @@ const courseSettings = {
 const productName = courseSettings.name || 'Unamed Product'
 const { name = 'Unamed Product' } = courseSettings
 
+console.group('Usando short circuit con el operador lógico ó usar desestructurado  con valores por defecto.')
+  console.log(productName)
+  console.log(name)
+console.groupEnd()
+
 /**
  * Valores que pueden ser malinterpretados
  */
 /**
- * courseSettings.shortName ('') evalua false pero
+ * courseSettings.shortname ('') evalua false pero
  * ¿Y si lo que quiero es obtener es la cadena vacia?
  */
-const shortName = courseSettings.shortName || 'No short Name'
+const shortname = courseSettings.shortname || 'No short Name'
 /**
  * courseSettings.discount (0) evalua a false pero
  * ¿Y si lo que quiero es obtener el número 0?
@@ -44,7 +49,24 @@ const discountNumber = courseSettings.discount || 10
  */
 const showPublictly = courseSettings.showPublictly || true
 
+
+console.group('Valores que pueden ser malinterpretados.')
+  console.log('shortname es ' + shortname + ' ,¿Y si lo que quiero es obtener es la cadena vacia?' )
+  console.log('discountNumber es '+ discountNumber + ', ¿Y si lo que quiero es obtener el número 0?' )
+  console.log('showPublictly es '+ showPublictly + ', ¿Y si lo que quiero es obtener el valor false?' )
+console.groupEnd()
+
+
 /**
  * Ej, Nullish Coalescing Operator (??) - ESNext - Bits
  */
 
+const courseShortname = courseSettings.shortname ?? 'No short Name'
+const discount = courseSettings.discount ?? 10
+const published = courseSettings.showPublictly ?? true
+
+console.group('Ej, Nullish Coalescing Operator (??) - ESNext - Bits.')
+  console.log(`courseShortname es una cadena vacia '${courseShortname}'`)
+  console.log('discount es '+ discountNumber)
+  console.log('published es '+ published)
+console.groupEnd()
